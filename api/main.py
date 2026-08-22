9bd038518fc39eb5af4b695f5bb003242f0e1779
mateo <manherran2.6@gmail.com>
2026-08-21 23:42:39 -0500

    Coreccion de frontend con vercel.json

--- api/main.py
+++ api/main.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parent))
+BASE_DIR = Path(__file__).resolve().parent
 sys.path.append(str(BASE_DIR))
 
 from fastapi import FastAPI
